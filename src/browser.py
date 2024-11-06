03ae7a329a99be485e49dd9319036f2e6ccc4eaf
Vitor <viisanches98@gmail.com>
2024-11-06 16:50:51 -0300

    feat: add up and down keyboard arrow scrolling

--- src/browser.py
+++ src/browser.py
@@ -3,6 +3,7 @@ import tkinter
 
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 9, 18
+SCROLL_STEP = 100
 
 class Browser:
   def __init__(self):
@@ -13,6 +14,9 @@ class Browser:
       height=HEIGHT
     )
     self.canvas.pack()
+    self.scroll = 0
+    self.window.bind("<Down>", self.__scrolldown)
+    self.window.bind("<Up>", self.__scrollup)
 
   def load(self, url):
     http = HTTP(url)
@@ -22,8 +26,11 @@ class Browser:
     self.draw()
 
   def draw(self):
+    self.canvas.delete("all")
     for x, y, c in self.display_list:
-      self.canvas.create_text(x, y, text=c)
+      if y > self.scroll + HEIGHT: continue
+      if y + VSTEP < self.scroll : continue
+      self.canvas.create_text(x, y - self.scroll, text=c)
 
   def __layout(self, text):
     display_list=[]
@@ -47,3 +54,11 @@ class Browser:
       elif not in_tag:
         text += c
     return text
+
+  def __scrolldown(self, e):
+    self.scroll += SCROLL_STEP
+    self.draw()
+
+  def __scrollup(self, e):
+    self.scroll -= SCROLL_STEP
+    self.draw()
