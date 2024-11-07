f62c8bded54a02dbd51b4b73462a1576546cdcfa
Vitor <viisanches98@gmail.com>
2024-11-07 13:37:50 -0300

    feat: add support to different size texts in the same line

--- src/layout.py
+++ src/layout.py
@@ -5,36 +5,73 @@ from lex import Text, Tag
 
 HSTEP, VSTEP = 9, 18
 
+sizes = {
+  'h1': 16,
+  '/h1': 10,
+  'h2': 14,
+  '/h2': 10,
+  'h3': 12,
+  '/h3': 10,
+  'default': 10
+}
+weights = {
+  'strong': 'bold',
+  'b': 'bold',
+  '/strong': 'normal',
+  '/b': 'normal',
+  'default': 'normal',
+}
+slants = {
+  'i': 'italic',
+  'i': 'roman',
+  'default': 'roman'
+}
+
 class Layout:
   def __init__(self, tokens, dimensions):
     self.display_list = []
     self.cursor_x = HSTEP
     self.cursor_y = VSTEP
-    self.style = "roman"
-    self.weight = "normal"
+    self.style = slants['default']
+    self.weight = weights['default']
+    self.size = sizes['default']
     self.width, _ = dimensions
-    for tok in tokens:
-      self.__token(tok)
+    self.line = []
+    for tok in tokens: self.__token(tok)
+    self.__flush()
 
   def __token(self, tok):
     font = tkinter.font.Font(
       family="Inter",
-      size=10,
+      size=self.size,
       weight=self.weight,
       slant=self.style
     )
     if isinstance(tok, Text):
       for word in tok.text.split():
         self.__word(word, font)
-    elif tok.tag == "i": self.style = "italic"
-    elif tok.tag == "/i": self.style = "roman"
-    elif tok.tag in ["b", "strong"]: self.weight = "bold"
-    elif tok.tag in ["/b", "/strong"]: self.weight = "normal"
+    else:
+      if tok.tag in sizes: self.size = sizes[tok.tag]
+      if tok.tag in slants: self.style = slants[tok.tag]
+      if tok.tag in weights: self.weight = weights[tok.tag]
+      if tok.tag in ['br', '/p', '/h1', '/h2', '/h3', '/aside']: self.__flush()
 
   def __word(self, word, font):
     w = font.measure(word)
-    if (self.cursor_x + w > self.width - HSTEP) or (word == "\n"):
-      self.cursor_y += font.metrics("linespace") * 1.25
-      self.cursor_x = HSTEP
-    self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+    if (self.cursor_x + w > self.width - HSTEP):
+      self.__flush()
+    self.line.append((self.cursor_x, word, font))
     self.cursor_x += w + font.measure(" ")
+
+  def __flush(self):
+    if not self.line: return
+    metrics = [font.metrics() for x, word, font in self.line]
+    max_ascent = max([metric["ascent"] for metric in metrics])
+    baseline = self.cursor_y + 1.25 * max_ascent
+    for x, word, font in self.line:
+      y = baseline - font.metrics("ascent")
+      self.display_list.append((x, y, word, font))
+    max_descent = max([metric["descent"] for metric in metrics])
+    self.cursor_y = baseline + 1.25 * max_descent
+    self.cursor_x = HSTEP
+    self.line = []
